d2508cd4e4ac6341020f16096ab01bb7b670aac2
Antoine-Jo <83338682+Antoine-Jo@users.noreply.github.com>
2021-11-30 19:21:19 +0100

    salt/hash pwd avant insert bdd

--- backend/controllers/user_controllers.js
+++ backend/controllers/user_controllers.js
@@ -1,10 +1,16 @@
 const {insertUser} = require('../models/User_models');
+const bcrypt = require('bcrypt');
 
 
 exports.signup = async (req, res, next) => {
     try {
-        const name = req.body.name; const firstname = req.body.firstname; const email = req.body.email; const password = req.body.password;
-        await insertUser(name, firstname, email, password)
+        const name = req.body.name.trim(); const firstname = req.body.firstname.trim(); const email = req.body.email.trim(); const password = req.body.password.trim();
+        if (name == "" || firstname == "" || email == "" || password == "") {
+            return res.status(400).json({ message: 'Les champs doivent être remplis !'})
+        }
+        const salt = await bcrypt.genSalt();
+        pwdhash = await bcrypt.hash(password, salt);
+        await insertUser(name, firstname, email, pwdhash)
         res.status(200).json({message: 'Utilisateur créé !'})
     }
     catch(err) {
