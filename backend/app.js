f2b8adfb27206d897cc50db2efb9bbb710055d4a
Antoine-Jo <83338682+Antoine-Jo@users.noreply.github.com>
2021-12-03 09:26:24 +0100

    Update App.js

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,10 @@ const app = express();
 app.use(helmet());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Header qui permet d'accéder à notre API depuis n'importe quelle origine
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Header qui permet d'accéder à notre API depuis n'importe quelle origine
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); //Ajouter les headers mentionnés aux requêtes envoyées vers notre API
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // d'envoyer des requêtes avec les méthodes mentionnées
+    res.setHeader("Access-Control-Allow-Credentials", "true");
     next();
 });
 
