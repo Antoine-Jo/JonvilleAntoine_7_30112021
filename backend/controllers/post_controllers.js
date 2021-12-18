49ecd67679743d33aab5dd02519b85d8d6464349
Antoine-Jo <83338682+Antoine-Jo@users.noreply.github.com>
2021-12-18 17:15:53 +0100

    Update Likes

--- backend/controllers/post_controllers.js
+++ backend/controllers/post_controllers.js
@@ -103,19 +103,18 @@ const likePost = async (req, res) => {
     const token = req.cookies.jwt;
     const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
     const userId = decodedToken.id;
-    // console.log(postId);
+
     const likes = await getLike(userId, postId)
-    console.log(likes);
-    // if (!likes) throw {status:404, msg : "Ce post est introuvable !" };
 
     if (likes === 0 ) {
       await createLike(userId, postId)
-      return res.status(200).send({ message: "Le post est like !"})
+      return res.status(200).json({likes})
+      // send({ message: "Le post est like !"})
     } else {
       await deleteLike(userId, postId)
-      return res.status(200).send({ message: "Le post a été dislike !"})
+      return res.status(200).json({likes})
+      // send({ message: "Le post a été dislike !"})
     }
-    // console.log(likes);
   } catch (err) {
     return res
     .status(err.status ? err.status : 500)
